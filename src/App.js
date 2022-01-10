717e573186c88ee3e9c3ef7075fada210554d10a
Prabhnometery <pssbadwa@uwaterloo.ca>
2022-01-10 18:40:53 -0500

    created card component and added styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./App.css";
 
 // Import Components
 import Header from "./components/Header";
+import CollectionCard from "./components/CollectionCard";
+
+
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard id={0} name={'Bandana Punk'} traits={[{'value' : 7}]} image={''}/>
     </div>
   );
 }
---
+++ src/components/CollectionCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import './CollectionCard.css'
+
+// Assests
+import weth from '../assets/weth.png'
+
+const CollectionCard = ({id, name, traits, image}) => {
+    return (
+        <div className='collectionCard'>
+            <img src={image} className='cardImage' />
+            <div className='details'>
+            <div className='name'>
+                {name} <div className='id'>#{id}</div>
+            </div>
+            
+            <div className='priceContainer'>
+                <img src={weth} className='wethImage' />
+                <div className='price'>{traits[0]?.value}</div>
+            </div>
+            </div>
+            
+            
+        </div>
+    )
+}
+
+export default CollectionCard
