3706ea93111dc5e03104cbaf8d317d8a6e748aa7
Prabhnometery <pssbadwa@uwaterloo.ca>
2022-01-10 02:07:32 -0500

    header completed and styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import React from "react";
 import "./Header.css";
 import punkLogo from "../assets/header/cryptopunk-logo.png";
 import searchIcon from "../assets/header/search.png";
+import switchIcon from "../assets/header/theme-switch.png"
 
 const Header = () => {
   return (
@@ -20,10 +21,20 @@ const Header = () => {
       </div>
 
       <div className="headerItems">
-          <p>Drops</p>
-          <p>Marketplace</p>
-          <p>Create</p>
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
 
+      <div className="headerActions">
+          <div className="themeSwitchContainer">
+              <img src={switchIcon} />
+          </div>
+
+      </div>
+
+      <div className="loginButton">
+          GET IN 
       </div>
     </div>
   );
