3f6a53b80b6f67da2fa3d791d81e99eb1d89205d
Prabhnometery <pssbadwa@uwaterloo.ca>
2022-01-10 01:12:29 -0500

    init

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h2>I am the Header</h2>
+            
+        </div>
+    )
+}
+
+export default Header
