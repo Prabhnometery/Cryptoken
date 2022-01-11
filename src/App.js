196f0e48cef022acf4853122f3affe5896d7f389
Prabhnometery <pssbadwa@uwaterloo.ca>
2022-01-10 19:18:47 -0500

    api call to opensea, created PunkList component to pass collection cards as an array

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 // Import Components
 import Header from "./components/Header";
 import CollectionCard from "./components/CollectionCard";
-
-
+import PunkList from "./components/PunkList";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  // API CALL
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openSeaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xFc129B9daffBB97464488c12664593c3632df8DF"
+      );
+      console.log(openSeaData.data.assets);
+      setPunkListData(openSeaData.data.assets);
+    };
+    return getMyNfts;
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <CollectionCard id={0} name={'Bandana Punk'} traits={[{'value' : 7}]} image={''}/>
+      {/* <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image={""}
+      /> */}
+      <PunkList punkListData={punkListData} />
     </div>
   );
 }
